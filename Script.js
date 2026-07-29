2965bdd37181c80f21a243019589cf5509df0302
gokuxdeku528-del <gokuxdeku528@gmail.com>
2026-07-29 11:50:25 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -124,12 +124,156 @@ return;
 
 }
 
-alert(
-"Decision:\\n"+
-decision+
-"\\n\\nOptions:\\n"+
-options.join("\\n")
-);
+showCategoryScreen(decision,options);
+
+};
+
+    }
+function showCategoryScreen(decision, options){
+
+const defaultCategories=[
+"Price",
+"Performance",
+"Camera",
+"Battery",
+"Display"
+];
+
+app.innerHTML=`
+
+<div class="screen">
+
+<h1>Compare By</h1>
+
+<p class="subtitle">${decision}</p>
+
+<div id="categoryBox"></div>
+
+<button id="addCategory">
++ Add Category
+</button>
+
+<br><br>
+
+<button id="continueCategory">
+Continue
+</button>
+
+</div>
+
+`;
+
+const box=document.getElementById("categoryBox");
+
+defaultCategories.forEach(name=>{
+
+const input=document.createElement("input");
+
+input.className="categoryInput";
+
+input.value=name;
+
+box.appendChild(input);
+
+});
+
+document.getElementById("addCategory").onclick=()=>{
+
+const input=document.createElement("input");
+
+input.className="categoryInput";
+
+input.placeholder="Custom Category";
+
+box.appendChild(input);
+
+};
+
+document.getElementById("continueCategory").onclick=()=>{
+
+const categories=[...document.querySelectorAll(".categoryInput")]
+
+.map(i=>i.value.trim())
+
+.filter(Boolean);
+
+showWeightScreen(decision,options,categories);
+};
+
+    }
+function showWeightScreen(decision,options,categories){
+
+app.innerHTML=`
+<div class="screen">
+
+<h1>Importance</h1>
+
+<p class="subtitle">
+Move the sliders
+</p>
+
+<div id="weightBox"></div>
+
+<button id="finishWeight">
+Continue
+</button>
+
+</div>
+`;
+
+const box=document.getElementById("weightBox");
+
+categories.forEach(category=>{
+
+const item=document.createElement("div");
+
+item.className="weightItem";
+
+item.innerHTML=`
+
+<p>${category}
+<span id="${category}Value">5</span>/10
+</p>
+
+<input
+type="range"
+min="1"
+max="10"
+value="5"
+class="weightSlider">
+
+`;
+
+box.appendChild(item);
+
+const slider=item.querySelector(".weightSlider");
+const value=item.querySelector("span");
+
+slider.oninput=()=>{
+
+value.textContent=slider.value;
+
+};
+
+});
+
+document.getElementById("finishWeight").onclick=()=>{
+
+const weights={};
+
+document.querySelectorAll(".weightItem").forEach(item=>{
+
+const name=item.querySelector("p").childNodes[0].textContent.trim();
+
+const value=item.querySelector("input").value;
+
+weights[name]=Number(value);
+
+});
+
+console.log(weights);
+
+alert("Importance Saved!");
 
 };
 
