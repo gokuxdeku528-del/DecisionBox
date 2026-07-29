64361bf17a9791b7d023a5300041409035b57c94
gokuxdeku528-del <gokuxdeku528@gmail.com>
2026-07-29 11:40:46 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,65 @@
+const app = document.getElementById("app");
 
+app.innerHTML = `
+<div class="screen">
+
+<div class="logo">🧠</div>
+
+<h1>DecisionBox</h1>
+
+<p class="subtitle">
+Can't decide?<br>
+Let's figure it out together.
+</p>
+
+<button id="startBtn">
+Start Decision
+</button>
+
+</div>
+`;
+
+document.getElementById("startBtn").addEventListener("click", () => {
+    showDecisionScreen();
+});
+
+function showDecisionScreen(){
+
+app.innerHTML = `
+<div class="screen">
+
+<h1>New Decision</h1>
+
+<p class="subtitle">
+What are you trying to decide?
+</p>
+
+<input
+id="decisionInput"
+type="text"
+placeholder="Example: Which phone should I buy?">
+
+<button id="continueBtn">
+Continue
+</button>
+
+</div>
+`;
+
+document
+.getElementById("continueBtn")
+.addEventListener("click", () => {
+
+const decision =
+document.getElementById("decisionInput").value.trim();
+
+if(decision===""){
+alert("Please enter your decision.");
+return;
+}
+
+alert("Decision Saved:\\n\\n"+decision);
+
+});
+
+}
